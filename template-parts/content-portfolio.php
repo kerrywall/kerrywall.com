ea1039ecf8abe3feefa484f937ceed81fa53dd6b
kerrywall <kerry.wall@gmail.com>
2018-11-04 22:38:55 -0500

    adjusting portfolio single

--- template-parts/content-portfolio.php
+++ template-parts/content-portfolio.php
@@ -12,15 +12,17 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<p class="summary"><?php echo get_field('description'); ?></p>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<div class="image clearfix">
 			<?php echo get_the_post_thumbnail(); ?>	
 		</div>
-		<p class="summary"><?php echo get_field('description'); ?></p>
-		<?php the_content(); ?>	
-		<p class="link"><a href="<?php echo get_field('url') ?>" target="_blank">Visit <?php echo get_the_title(); ?></a>.</p>
+		<div class="text">
+			<?php the_content(); ?>	
+			<p class="link"><a href="<?php echo get_field('url') ?>" target="_blank">Visit <?php echo get_the_title(); ?></a> (link will open in a new window).</p>
+		</div>
 	</div><!-- .entry-content -->
 
 </article><!-- #post-## -->
