55f39cb82e459b69689533101ca1c0c288b34b02
kerrywall <kerry.wall@gmail.com>
2018-11-04 22:37:21 -0500

    text tweak

--- front-page.php
+++ front-page.php
@@ -16,14 +16,13 @@ get_header(); ?>
 
 	<div id="primary" class="about">
 		<h2>hi</h2>
-		<p>I'm a web developer, web manager, web writer, web-accessibilty enthusiast, and generally a web fan. I first dabbled in code in 1999, while in high school, and have had web-centric jobs at Ryerson University, CBC News and, currently, the Toronto Symphony Orchestra. I have more than 10 years of professional web experience in the areas listed above.</p>
+		<p>I'm a web developer, web manager, web writer, web-accessibilty enthusiast, and generally a web fan. I first dabbled in code in 1999, while in high school.</p>
+		<p>I have more than 10 years of professional web experience in the areas listed above, as well as a journalism degree (with a major in print) from Ryerson University. I completed a certificate in digital media skills from OCAD University in 2012, and HackerYou's full-time front-end development bootcamp in 2014.</p>
+		<p>I'm also a runner, knitter, and amateur pianist. I'm available for freelance web development and/or consulting.</p>
 	</div><!-- #primary -->
-	<div class="work">
-		<h2>work</h2>
-		<p>Lorem ipsum dolor sit amet, <a href="#">consectetur adipisicing elit</a>. Repudiandae praesentium similique ut aut odio cupiditate unde aliquid tempora expedita, porro eveniet quidem! Ea recusandae tempora iste illo quidem at voluptate!</p>
-	</div>
 	<div class="projects">
 		<h2>projects</h2>
+		<p>These are a mix of professional undertakings and personal code experiments.</p>
 		<?php
 
 		// The Query
@@ -35,10 +34,11 @@ get_header(); ?>
 		// The Loop
 		if ( $the_query->have_posts() ) {
 			while ( $the_query->have_posts() ) {
-				echo '<div class="project">';
+				echo '<div class="project clearfix">';
 				$the_query->the_post();
-				echo '<img src="http://placecorgi.com/600/300">';
-				echo '</div>';
+				echo '<a href="' . get_the_permalink($the_query->post->ID) . '"><div class="image">' . get_the_post_thumbnail() . '</div>';
+				echo '<h3>' . get_the_title($the_query->post->ID) . '</h3>';
+				echo '</a></div>';
 			}
 			
 			/* Restore original Post Data */
@@ -49,7 +49,7 @@ get_header(); ?>
 	</div>
 	<div class="freelance">
 		<h2>get in touch</h2>
-		<p>I'm available to discuss freelance web projects, and can be reached at <a href="mailto:kw@kerrywall.com">kw@kerrywall.com</a>.</p>
+		<p>I'm available to discuss freelance web projects, and can be reached at <a href="mailto:kw@kerrywall.com">kw@kerrywall.com</a>, on <a href="https://www.linkedin.com/in/kerry-wall-1bb4304/" target="_blank">LinkedIn</a>, and on <a href="https://twitter.com/kerrywall" target="_blank">Twitter</a>.</p>
 	</div>
 
 <?php
